9be4b7b63554873f3cc457dc03c2af7a48c2852c
Jebermudez <118839632+ItoIto409@users.noreply.github.com>
2024-09-04 18:00:19 -0500

    Update Tarjeta.java
    Añadido el atributo time que registra la hora de creación de una tarjeta

--- src/ProyectoEstructuras/Tarjeta.java
+++ src/ProyectoEstructuras/Tarjeta.java
@@ -2,6 +2,7 @@ package ProyectoEstructuras;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDateTime;
 
 public class Tarjeta extends Lista<String> {
 
@@ -10,19 +11,29 @@ public class Tarjeta extends Lista<String> {
     private String tag;
     private float rating;
     public List<Float> ratings = new ArrayList<>();
+    private LocalDateTime time;
+
 
     public Tarjeta(String data, String title, String tag, float rating) {
         this.data = data;
         this.title = title;
         this.tag = tag;
         this.rating = rating;
+        this.time = LocalDateTime.now();
     }
 
     public Tarjeta(String data, String title, String tag) {
         this.data = data;
         this.title = title;
         this.tag = tag;
+        this.time = LocalDateTime.now();
     }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    
 
     public String getData() {
         return data;
